d3c812ce77be8dbde99bdfea782d9257270a81e8
Patrick-dev <poc.caiza@yavirac.edu.ec>
2023-12-08 23:14:05 -0500

    task: moodle - create category from - list

--- src/app/pages/moodle/moodle-routing.module.ts
+++ src/app/pages/moodle/moodle-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CategoryFormComponent } from './category/category-form/category-form.component';
+import { CategoryListComponent } from './category/category-list/category-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: ':id',
+    component: CategoryListComponent
+  },
+  {
+    path: '',
+    component: CategoryFormComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
